9050af8dfc0f5627ab9a633a66fa3ec824966579
antoniofaienza93 <afaienza93@gmail.com>
2018-06-27 09:43:05 +0200

    [GRUNT] Add grunt file

---
+++ Gruntfile.js
@@ -0,0 +1,10 @@
+
+
+// https://www.youtube.com/watch?v=TMKj0BxzVgw
+// npm install -S grunt 
+module.exports = function(grunt) {
+
+    grunt.registerTask('speak', function(){
+        console.log("CIAO MAMMA GUARDA QUANTO MI DIVERTO");
+    });
+};
