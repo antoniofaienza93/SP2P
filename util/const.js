79e5f0337f5a62d4e0e40ae1593be1cd9bf1cfc5
antoniofaienza93 <afaienza93@gmail.com>
2018-08-30 17:48:33 +0200

    update const.js

--- util/const.js
+++ util/const.js
@@ -51,7 +51,7 @@ const PEER_SERVER = {
  *      PATH: "/peerjs"
  */
 const PEER_CLIENT = {
-    HOST: "https://project-p2p.herokuapp.com/",
+    HOST: location.hostname,
     PORT: location.port || (location.protocol === 'https:' ? 443 : 80),
     PATH: "/peerjs"
 }
