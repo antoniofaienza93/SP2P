39cd80623feac0de6d8025868b7f10b32a1ad5d1
antoniofaienza93 <afaienza93@gmail.com>
2018-08-30 18:17:26 +0200

    clear Code

--- P2P/managePeerCommunication.js
+++ P2P/managePeerCommunication.js
@@ -66,7 +66,6 @@ window.onload = function () {
         var input = document.getElementById("inputid");
 
         if (input.checkValidity()) {
-            console.log(data);
 
             document.getElementById("connect").disabled = true;
             input.disabled = true;
