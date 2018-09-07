a52ba14e949e87757ffad849d6030ec77a74ea53
antoniofaienza93 <afaienza93@gmail.com>
2018-09-07 13:41:53 +0200

    Change PORT

--- util/const.js
+++ util/const.js
@@ -50,7 +50,7 @@ const PEER_SERVER = {
  */
 const PEER_CLIENT = {
     HOST: "project-p2p.herokuapp.com",
-    PORT: 3000, //|| (location.protocol === 'https:' ? 443 : 80)
+    PORT: 16782, //|| (location.protocol === 'https:' ? 443 : 80)
     PATH: "/peerjs"
 }
 
