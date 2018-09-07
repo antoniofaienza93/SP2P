c1b2c2a54d4b21790f2db2a3f85de788ecb10a60
antoniofaienza93 <afaienza93@gmail.com>
2018-09-07 13:50:19 +0200

    Update PeerClient

--- P2P/PeerClient.js
+++ P2P/PeerClient.js
@@ -20,11 +20,6 @@ class PeerClient {
         this._path = pth;
         this._conn = undefined;
 
-        console.log("location ");
-        console.log(location);
-        console.log("host ----> " + h);
-        console.log("port ----> " + p);
-        console.log("path ----> " + pth);
         this._peer = new Peer(this._id, {
             host: this._host, // 'localhost',
             port: this._port, //  9000,
